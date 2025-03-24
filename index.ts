c114c5749f735892fca0c397d1c38a9e5c7e0cbb
Trevor Miller <trevormil@comcast.net>
2025-03-24 07:10:59 -0400

    bump

--- index.ts
+++ index.ts
@@ -9,10 +9,13 @@ const port = 5000;
 //typically with env variables
 const BITBADGES_CLIENT_ID = process.env.BITBADGES_CLIENT_ID ?? '';
 const BITBADGES_CLIENT_SECRET = process.env.BITBADGES_CLIENT_SECRET ?? '';
+const BITBADGES_API_KEY = process.env.BITBADGES_API_KEY ?? '';
 // const BITBADGES_CLIENT_ID =
 //     '0bd8c06544c539f067d89cb50c3b0731033876ae0b3936dd897796abc1a7b540';
 // const BITBADGES_CLIENT_SECRET =
 //     '8e901787d8a5fa2ba22b0c026486f9e42a49b509a5db9414d7e484e59ba6e9fd';
+// const BITBADGES_API_KEY =
+//     '0bd8c06544c539f067d89cb50c3b0731033876ae0b3936dd897796abc1a7b540';
 
 app.use(
     session({
@@ -39,6 +42,9 @@ passport.use(
             clientID: BITBADGES_CLIENT_ID,
             clientSecret: BITBADGES_CLIENT_SECRET,
             callbackURL: `http://localhost:${port}/auth/bitbadges/callback`,
+            customHeaders: {
+                'x-api-key': BITBADGES_API_KEY,
+            },
         },
         function (
             accessToken: string,
@@ -46,9 +52,9 @@ passport.use(
             profile: any,
             cb: any
         ) {
-            const address = accessToken;
-            const user = { address, id: address };
-            console.log('user', user);
+            const user = {};
+
+            // TODO: Fetch user info as needed
             return cb(null, user);
         }
     )
@@ -69,8 +75,10 @@ app.get(
     '/auth/bitbadges/callback',
     passport.authenticate('oauth2', { failureRedirect: '/login' }),
     function (req, res) {
+        // TODO: Check your claims or any other logic you need to perform here
+
         // Successful authentication, redirect home.
-        res.redirect('/profile');
+        return res.redirect('/profile');
     }
 );
 
